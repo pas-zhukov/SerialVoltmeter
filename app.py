6880141dc010592d9c44a42a4ac0daeb39826039
pas-zhukov <pas-zhukov@yandex.ru>
2024-04-10 10:43:31 +0300

    continue

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import typing
 
+import matplotlib
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice
+from PyQt5.QtSerialPort import QSerialPort
+
+matplotlib.use('Qt5Agg')
 
 from models import VoltageRange, TimeUnits
 
@@ -10,6 +13,7 @@ from models import VoltageRange, TimeUnits
 class SerialVoltmeterApp(QtWidgets.QApplication):
     def __init__(self, argv: typing.List[str]):
         super().__init__(argv)
+        self.file = open("temp_data.txt", "a+")
         self.ui = uic.loadUi("mainForm.ui")
         self.ui.setWindowTitle("Serial Voltmeter")
         self.serial = QSerialPort()
@@ -17,43 +21,51 @@ class SerialVoltmeterApp(QtWidgets.QApplication):
 
         self.init_gui()
         self.serial.readyRead.connect(self.parse_serial)
+        self.lastWindowClosed.connect(self.file.close)
 
         self.ui.show()
         self.exec()
 
-        self.connect_device()
-
     def init_gui(self):
+        self.ui.connection.triggered.connect(self.connect_device)
+        self.ui.exit.triggered.connect(self.exit)
+        self.ui.startButton.clicked.connect(self.start_recording)
         self.ui.recordRange.addItems(VoltageRange.RANGES_STR)
         self.ui.samplingTimeUnits.addItems(TimeUnits.MAPPING.value.values())
         self.ui.recordLengthTimeUnits.addItems(TimeUnits.MAPPING.value.values())
-        self.ui.recordSampling.addItem(str(VoltageRange(6144).sampling))
+        self.ui.recordRange.currentTextChanged.connect(self.refresh_gui)
+        self.refresh_gui()
+
+    def refresh_gui(self):
+        self.ui.recordSampling.clear()
+        self.ui.recordSampling.addItem(
+            str(VoltageRange(VoltageRange.RANGES[self.ui.recordRange.currentIndex()]).sampling))
 
     def parse_serial(self):
         if not self.serial.canReadLine():
             return
         data = str(self.serial.readLine())[2:-5].strip().split(",")
-        if data[0] == 5:
-            with open("temp_data.txt", "a+") as file:
-                file.write(f"{data[1]},{data[2]},{data[3]}\n")
-        if data[0] == 1:
-            if data[1] == 1 and data[2] == 1:
-                dlg = QtWidgets.QDialog()
-                dlg.setWindowTitle("Прибор успешно подключен!")
-                dlg.exec()
+        self.ui.console.appendPlainText(str(data))
+        if data[0] == '5':
+            self.file.write(data.__str__() + "\n")
+        if data[0] == '0':
+            if data[1] == '1':
+                self.ui.console.appendPlainText("Прибор успешно подключен!")
+            elif data[1] == '0':
+                self.ui.console.appendPlainText("ОШИБКА: Не удалось инициализировать плату АЦП")
 
     def connect_device(self):
         # автопоиск порта здесь будет
         self.serial.setPortName("COM3")
         self.serial.open(QIODevice.ReadWrite)
-        self.send_package([0, 1, 1])
+        self.send_package([0, 0])
 
     def send_package(self, data: list[int]):
         txs = ','.join(map(str, data)) + '\n'
         self.serial.write(txs.encode())
 
     def start_recording(self):
-        self.send_package([4, 6144, 10, 60*1000])
+        self.send_package([4, 6144, 10, 1800])
 
 
 if __name__ == "__main__":
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,37 +1,62 @@
-#include "Parser.h"       // библиотека парсера
-#include "AsyncStream.h"  // асинхронное чтение сериал
+#include <ADS1115_WE.h>
+#include <Wire.h>
+
+#include "Parser.h"
+#include "AsyncStream.h"
+
+
+// SDA: PIN_A4
+// SCL: PIN_A5
+#define I2C_ADDRESS 0x48
+
+
+/*
+GLOBALS
+*/
 AsyncStream<50> serial(&Serial, ';');   // указываем обработчик и стоп символ
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
+
+
 int piezoPin = 11;
-bool measure_mode = false;
+int alertPin = 2;
+
+int sampling = 1000;
+long duration = 2L * 60L * 1000L;
+bool measMode = false;
+uint32_t tmr = 0;
 
 void setup() {
+  Wire.begin();
   Serial.begin(115200);
-}
-
-void melody() {
-  tone(piezoPin, 880, 70);
-  delay(70);
-  tone(piezoPin, 1109, 70);
-  delay(70);
-  tone(piezoPin, 1319, 70);
+  adc.setMeasureMode(ADS1115_CONTINUOUS);
+  adc.setConvRate(ADS1115_860_SPS);
 }
 
 void loop() {
   parsing();
-  static uint32_t tmr = 0;
-  if (micros() - tmr >= 1000 && measure_mode) {
-    tmr = micros()();
-    Serial.print(0);
+  if (millis() - tmr >= sampling && duration > 0) {
+    duration -= sampling;
+    tmr = millis();
+    Serial.print(5);
     Serial.print(',');
-    Serial.print(analogRead(4));
+    Serial.print(millis());
     Serial.print(',');
-    Serial.print("5.136");
+    Serial.print(readChannel(ADS1115_COMP_0_1));
     Serial.print(',');
-    Serial.println(millis());
-  }
+    Serial.print(readChannel(ADS1115_COMP_2_3));
+    Serial.print(',');
+    Serial.println(duration);
+  } 
 }
 
-// функция для отправки пакета на ПК
+
+float readChannel(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  voltage = adc.getResult_mV();
+  return voltage;
+}
+
 void sendPacket(int key, int* data, int amount) {
   Serial.print(key);
   Serial.print(',');
@@ -50,17 +75,62 @@ void parsing() {
     data.parseInts(ints);   // парсим в него
     switch (ints[0]) {      // свитч по ключу
       case 0:  // Проверка подключения
+        if (!adc.init()) {
+          Serial.println("0,0");
+          error();
+          break;
+        }
         tone(piezoPin, 1300, 100); // Пикаем
-        Serial.println("1,1,1");
+        Serial.println("0,1");
         break;
-      case 1: // Настройка параметров записи
+      case 1:
         break;
       case 2:
         break;
       case 3:
         break;
       case 4: //запрос на измерения
+        melody();
+        startRecording(ints[1], ints[2], ints[3]);
         break;
     }
   }
+}
+
+void startRecording(int range, int sampl, int dur) {
+  switch (range) {
+    case 6144:
+      adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+    case 4096:
+      adc.setVoltageRange_mV(ADS1115_RANGE_4096);
+    case 2048:
+      adc.setVoltageRange_mV(ADS1115_RANGE_2048);
+    case 1024:
+      adc.setVoltageRange_mV(ADS1115_RANGE_1024);
+    case 512:
+      adc.setVoltageRange_mV(ADS1115_RANGE_0512);
+    case 256:
+      adc.setVoltageRange_mV(ADS1115_RANGE_0256);
+  }
+
+  sampling = sampl;
+  duration = dur * 1000L;
+  measMode = 1;
+  Serial.println(range);
+  Serial.println(sampl);
+  Serial.println(dur);
+}
+
+void melody() {
+  tone(piezoPin, 880, 70);
+  delay(70);
+  tone(piezoPin, 1109, 70);
+  delay(70);
+  tone(piezoPin, 1319, 70);
+}
+
+void error() {
+  tone(piezoPin, 1000, 100);
+  delay(120);
+  tone(piezoPin, 950, 120);
 }
